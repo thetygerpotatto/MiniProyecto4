2686f3d88b030fb115c1abd7f2e95228ba006f80
thetygerpotatto <letras.cj@gmail.com>
2024-06-18 20:28:08 -0500

    Se añadio la habilidad de guardar estudiantes por nombre y nota

--- punto1/src/promedios/Promedios.java
+++ punto1/src/promedios/Promedios.java
@@ -1,15 +1,23 @@
 //?Imports____________________________________________________
 package promedios;
 import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane; //Joptionpane
 import javax.swing.JTextField;
+import javax.swing.table.TableModel;
+import javax.swing.JScrollPane;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
-
+import java.util.Set;
+import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.Scrollbar;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -21,29 +29,57 @@ public class Promedios extends JFrame implements ActionListener
     // *Creacion de los campos de texto e inputs
     JFrame window;
     Container container;
+    JPanel inputPane;
     JTextField gradeInput;
     JTextField nameInput;
     JButton submmit;
+    JLabel info;
+    JScrollPane ScrollList;
+    JTable studentList;
+    StudentTable tableModel;
+
+    String[][] studentInfo = {{"", ""}};
+    String[] columnNames = {"Nombre", "Nota"};
+
+    int studentInfoArraySize = 0;
     
     Map<String, Float> data;//<-------
 
     //?Clase promedios, aqui se crea la ventana________________
     public Promedios() 
     {
+        setLayout(new BorderLayout());
+
         container = getContentPane();
+        
+        inputPane = new JPanel();
+
         gradeInput = new JTextField();
         gradeInput.setColumns(20);
+
         nameInput = new JTextField();
         nameInput.setColumns(20);
+
         submmit = new JButton("Añadir estudiante");
+
         data = new HashMap<>();
-        setLayout(new FlowLayout());
+        
+        tableModel = new StudentTable(studentInfo, columnNames, 1);
+        studentList = new JTable(tableModel);
+        ScrollList = new JScrollPane(studentList);
+        
+        info = new JLabel("", JLabel.CENTER);
+
         
         submmit.addActionListener(this);
 
-        container.add(nameInput);
-        container.add(gradeInput);
-        container.add(submmit);
+        inputPane.add(nameInput);
+        inputPane.add(gradeInput);
+        inputPane.add(submmit);
+        inputPane.add(ScrollList);
+
+        container.add(inputPane, BorderLayout.CENTER);
+        container.add(info, BorderLayout.SOUTH);
         
         setVisible(true);
         setSize(600,450);
@@ -64,11 +100,35 @@ public class Promedios extends JFrame implements ActionListener
     @Override
     public void actionPerformed(ActionEvent e) {
         //si usuario valio añadir a hashmap nameimput = key, gradeimput = cont
+        Float grade;
         String name = nameInput.getText();
+
         try {
-            Float grado = Float.parseFloat(gradeInput.getText());
+            grade = Float.parseFloat(gradeInput.getText());
         } catch (NumberFormatException ex) {
-            
+            info.setText("el numero ingresado es invalido");
+            return;
         }
+
+        data.put(name, grade);
+        System.out.println(data.size());
+        updateStudentTable();
     }
+
+    private void updateStudentTable() {
+        studentInfo = new String[data.size()][2];
+        Iterator<String> keys = data.keySet().iterator();
+        int i = 0;
+        while (keys.hasNext()) {
+            String name = keys.next();
+            studentInfo[i][0] = name;
+            studentInfo[i][1] = "" + data.get(name);
+            ++i;
+        }
+        System.out.println("i=" + i);
+      
+        tableModel.changeData(studentInfo, data.size());
+        tableModel.fireTableStructureChanged();
+
+    } 
 }
